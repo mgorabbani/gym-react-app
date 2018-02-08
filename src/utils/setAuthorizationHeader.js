dec832efdba4863f491df9c0c0254c0cb40204a1
Golam Rabbani <knight3rrantt@gmail.com>
2018-02-08 15:53:21 +0600

    react-create-app on Heroku

--- src/utils/setAuthorizationHeader.js
+++ src/utils/setAuthorizationHeader.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default (token = null) => {
+  axios.defaults.baseURL = 'https://gymxpert-api.herokuapp.com/';
   if (token) {
     axios.defaults.headers.common.authorization = `Bearer ${token}`;
   } else {
